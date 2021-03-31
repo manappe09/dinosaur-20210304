9e31ee011a8efce97ced3d5c94de25cc08f42609
manappe09 <manape130@gmail.com>
2021-04-01 04:48:55 +0900

    Swiper実装

--- src/index.js
+++ src/index.js
@@ -5,3 +5,19 @@ import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
 // @import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&display=swap');
+// // import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+  
+// init Swiper:
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+   delay: 3000,
+ },
+});
